acd20e61607dd8bd795e12128823049fb85077f3
pradeepgr744 <dseeeap2020@gmail.com>
2023-11-21 20:54:46 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -37,11 +37,11 @@ document.execCommand("copy")
   return (
     <>
     
-        <div className='p-30 shadow-2xl hover:shadow-gray-800 mx-30 text-center p-3 bg-gray-800 rounded-md'>
-          <h1 className=' text-white '>Password Generator</h1>
-          <div className='flex justify-between overflow-hidden h-6 rounded-lg bg-white'>
+        <div className='max-lg:w-[30vw] max-lg:h-[43vh] max-lg:w-[50vw] max-lg:h-[43vh] max-sm:w-full p-30 shadow-2xl my-3 hover:shadow-gray-800 mx-30 text-center p-3 bg-gray-800 rounded-md'>
+          <h1 className='text-3xl text-white '>Password Generator</h1>
+          <div className='flex justify-between overflow-hidden my-3 h-10 rounded-2xl bg-white'>
             <input
-              className='bg-white mt-0 outline-none align-middle w-full text-black px-1 text-left'
+              className='bg-white mt-0 outline-none align-middle w-full text-2xl text-black px-3 text-left'
               id="dis"
               type='text'
               value={Password}
@@ -49,14 +49,14 @@ document.execCommand("copy")
               readOnly
             />
             <button 
-            className='font-md outline-none m-0 px-3 bg-[#577eff] text-white'
+            className='font-md outline-none m-0 px-3 bg-[#577eff] text-white text-2xl'
             onClick={()=>{
               copy(dis)
             }}
             >copy</button>
           </div >
           <div>
-            <button className='bg-green-400 m-3 rounded-2xl p-1 shadow-md hover:shadow-green-200'
+            <button className='bg-green-400 m-3 rounded-full p-1 shadow-md hover:shadow-green-200 text-2xl'
             onClick={()=>passwordGenerator()
            
             }
@@ -64,21 +64,21 @@ document.execCommand("copy")
           </div>
           
           <div className='m-1'>
-            <div className='flex items-center gap-x-1 text-sm text-white'>
+            <div className='flex items-center gap-x-1 text-2xl text-white'>
               <input
        
                 type='range'
                 min={6}
                 max={20}
                 value={length}
-                className='cursor-pointer scale-50 hover:scale-75'
+                className='cursor-pointer scale-50 hover:scale-100'
                 onChange={(e) => {
                   setlength(e.target.value);
                 }}
               />
               <label>Length:{length}</label>
             </div>
-            <div className='flex items-center gap-x-1 text-sm text-white'>
+            <div className=' flex items-center gap-x-1 m-2 text-2xl text-white'>
               <input
                 type='checkbox'
                 defaultChecked={numberAllowed}
@@ -89,7 +89,7 @@ document.execCommand("copy")
               />
               <label htmlFor='numberInput'>Numbers</label>
             </div>
-            <div className='flex items-center gap-x-1 text-sm text-white'>
+            <div className='flex items-center gap-x-1 m-2 text-2xl text-white'>
               <input
                 type='checkbox'
                 defaultChecked={charAllowed}
